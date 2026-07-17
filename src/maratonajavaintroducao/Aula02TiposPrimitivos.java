fc48455ff55491a830cca62fe036d3808c473899
Renata <cadisdsgn@gmail.com>
2026-07-17 20:15:21 -0300

    initial commit

---
+++ src/maratonajavaintroducao/Aula02TiposPrimitivos.java
@@ -0,0 +1,20 @@
+package maratonajavaintroducao;
+
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args){
+        // int, double, float, char, byte, short, long, boolean
+        int age = 10;
+        long numeroGrande = 100000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'F';
+        String nome = "Camila Cabello";
+
+        System.out.println("A idade e: " + age + " anos!");
+        System.out.println("O nome da cantora e: " + nome);
+    }
+}
---
+++ src/maratonajavaintroducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package maratonajavaintroducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String nome = "Camila";
+        String endereco = "Rua Cuba, n 18, Havana";
+        double salario = 3500;
+        String dataRecebimento = "17/07/2026";
+        String relatorio = "Eu " +nome+ ", morando no endereco " +endereco+ ", confirmo que recebi o salario de R$" +salario+ ", na data " +dataRecebimento+ ".";
+
+        System.out.println(relatorio);
+    }
+}
