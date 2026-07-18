3418d76286f0f095bc632796ba895071b7d5a536
Renata <cadisdsgn@gmail.com>
2026-07-18 13:29:38 -0300

    continuing on, day 2

---
+++ src/maratonajavaintroducao/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,15 @@
+package maratonajavaintroducao;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Doar de salario > 5000
+        double salario = 6000;
+
+        // (condicao) ? verdadeiro : falso;
+        String resultado = salario > 5000 ? "Eu vou doar 500 pro DevDojo" : "Ainda nao tenho condicoes, mas vou ter!";
+
+        System.out.println(resultado);
+
+
+    }
+}
